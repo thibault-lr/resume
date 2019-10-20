1e697ea125a53e66eb9629f665bea7ccf0742c65
thibault.leroy <thibault.le-roy@saegus.com>
2019-10-20 15:35:25 +0200

    add js and css linter

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -5,18 +5,18 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = WebpackMerge(common, {
-    mode: 'development',
-    devtool: 'source-map',
-    devServer: {
-        contentBase: path.join(__dirname, '', 'dist'),
-        watchContentBase: true,
-        historyApiFallback: true,
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-        }),
-    ],
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '', 'dist'),
+    watchContentBase: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
 });
